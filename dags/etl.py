dace87f4af98b42f73f1625539a4c95e6bf517eb
scg <simao.goncalves@priberam.pt>
2025-06-02 09:19:39 +0000

    update compose

--- dags/etl.py
+++ dags/etl.py
@@ -38,7 +38,6 @@ with DAG(
         postgres_hook.run(create_table_query)
 
     ## Step 2: Extract the NASA API Data(APOD)-Astronomy Picture of the Day[Extract pipeline]
-    ## https://api.nasa.gov/planetary/apod?api_key=CX29eEtrdrvk1fLGEikPiMapwlXfScQ5TjFyuMXY
     extract_apod = SimpleHttpOperator(
         task_id="extract_apod",
         http_conn_id="nasa_api",  ## Connection ID Defined In Airflow For NASA API
